e97bba878e017d32d5a599a4cd26216e3824d5e6
Ken Eucker <keneucker@gmail.com>
2017-09-30 10:11:57 -0700

    Placeholder images are now in the gallery along with a youtube video. All of the navigation is working correctly now and the call to actions are correctly linked. The ride number counts up! :)

--- templates/pdxbikesafe/js/scripts.js
+++ templates/pdxbikesafe/js/scripts.js
@@ -1,6 +1,9 @@
 (function($) {
     "use strict";
 
+    var rideCountupStarted = false,
+        originalCount = 0;
+
     $('body').scrollspy({
         target: '.navbar-fixed-top',
         offset: 60
@@ -13,6 +16,32 @@
     });
     
     new WOW().init();
+    
+    var isScrolledIntoView = function (elem) {
+        var docViewTop = $(window).scrollTop();
+        var docViewBottom = docViewTop + $(window).height();
+    
+        var elemTop = $(elem).offset().top;
+        var elemBottom = elemTop + $(elem).height();
+    
+        return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+    };
+
+    var startRideCountup = function () {
+        console.log('starting ride countup');
+        var counter = $('#ride-count');
+        var count = parseInt(counter.text());
+
+        if(originalCount <= count) {
+            return;
+        }
+        
+        counter.removeClass();
+        counter.text(count + 1);
+        counter.addClass('fadeInUp');
+
+        setTimeout(startRideCountup, 1);
+    };
     
     $('a.page-scroll').bind('click', function(event) {
         var $ele = $(this);
@@ -30,5 +59,17 @@
     $('#galleryModal').on('show.bs.modal', function (e) {
        $('#galleryImage').attr("src",$(e.relatedTarget).data("src"));
     });
+
+    $(window).scroll(function () {
+        var counter = $('#ride-count');
+        if(isScrolledIntoView(counter) && !rideCountupStarted) {
+            rideCountupStarted = true;
+            startRideCountup();
+        }
+    });;
+
+    var counter = $('#ride-count');
+    originalCount = parseInt(counter.text());
+    counter.text("0");
 
 })(jQuery);
